e957ffe440f17749e0bdd3a4db2a54f61c3e9b09
yu5kxk.g <yu5kxk.g@gmail.com>
2018-12-25 16:20:59 +0900

    [Update6]bookers2

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 
   attachment :user_image
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { minimum: 2, maximum: 20 }
   # validates :email, presence: true
   validates :introduction, length: { maximum: 50 }
 
