ec11e4d50dfa2d1db7763956b9bcc94e83dd6b6f
yu5kxk.g <yu5kxk.g@gmail.com>
2018-12-25 19:30:47 +0900

    [Update7]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,12 @@ class UsersController < ApplicationController
 
   def update
   	@user = User.find(params[:id])
-    @user.update_attributes(user_params)
-  	redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "Post was successfully created."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   def new #aboutページ
